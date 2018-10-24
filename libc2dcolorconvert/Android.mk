d9ba2d86c32c64dfed126882bd2d2b0eebc434a7
DennySPB <dennyspb@gmail.com>
2018-10-24 12:18:48 +0300

    msm8996: use generated kernel headers
    Change-Id: If389bfd1f4f5d576f4a6d7f387c97bffe65407fd
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -5,14 +5,13 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
         C2DColorConverter.cpp
 
-LOCAL_C_INCLUDES := \
-    $(TARGET_OUT_HEADERS)/adreno
+LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/adreno
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qcom/display
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 LOCAL_HEADER_LIBRARIES := \
         libutils_headers \
-        libhardware_headers
+        libhardware_headers \
+        generated_kernel_headers
 
 LOCAL_SHARED_LIBRARIES := liblog libdl
 
@@ -22,6 +21,5 @@ LOCAL_MODULE := libc2dcolorconvert
 
 LOCAL_VENDOR_MODULE := true
 
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
